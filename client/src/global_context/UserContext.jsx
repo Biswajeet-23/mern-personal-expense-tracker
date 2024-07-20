991135e185a676c142fc3f5d568c6aa540911886
Biswajeet Sahoo <2000biswajeet@gmail.com>
2024-07-20 11:53:05 +0530

    bug fixes

--- client/src/global_context/UserContext.jsx
+++ client/src/global_context/UserContext.jsx
@@ -8,10 +8,13 @@ export const UserProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
 
   const logout = async () => {
-    await fetch("http://127.0.0.4:4000/users/logout", {
-      method: "POST",
-      credentials: "include",
-    });
+    await fetch(
+      "https://mern-personal-expense-tracker-backend.onrender.com/users/logout",
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
     removeToken();
     setUserInfo(null);
   };
