f3fc76cae702d9d84bb04a6aa8e3149efbc4a2d9
Biswajeet Sahoo <2000biswajeet@gmail.com>
2024-07-20 12:37:04 +0530

    bug fixes

--- client/src/global_context/UserContext.jsx
+++ client/src/global_context/UserContext.jsx
@@ -1,11 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import useToken from "../custom_hook/useToken";
 
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children }) => {
   const { token, setToken, removeToken } = useToken();
-  const [userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState(() => {
+    const storedUserInfo = localStorage.getItem("userInfo");
+    return storedUserInfo ? JSON.parse(storedUserInfo) : null;
+  });
+
+  useEffect(() => {
+    if (userInfo) {
+      localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    } else {
+      localStorage.removeItem("userInfo");
+    }
+  }, [userInfo]);
 
   const logout = async () => {
     await fetch(
